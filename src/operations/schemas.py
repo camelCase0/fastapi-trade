bc7dbe20555d6935ce4b6756ad9ad4358a024c5f
camelCase0 <pashok2082.Ryuz@gmail.com>
2023-08-26 10:24:21 +0300

    ooperation.createSchema

--- src/operations/schemas.py
+++ src/operations/schemas.py
@@ -2,9 +2,7 @@ from datetime import datetime
 from pydantic import BaseModel
 
 class OperationCreate(BaseModel):
-    id: int
     quantity: str
     figi: str
     instrument_type: str
-    date: datetime
     type: str
